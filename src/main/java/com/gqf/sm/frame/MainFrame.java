3f9674ccb63e8544d28136064d8be32db7a9d4fd
guoqifeng <1138153570@qq.com>
2020-11-17 23:42:58 +0800

    java项目

--- src/main/java/com/gqf/sm/frame/MainFrame.java
+++ src/main/java/com/gqf/sm/frame/MainFrame.java
@@ -1,7 +1,9 @@
 package com.gqf.sm.frame;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import com.sun.org.apache.xml.internal.security.Init;
+
+import javax.swing.*;
+import java.awt.*;
 
 
 /**
@@ -12,9 +14,42 @@ import javax.swing.JPanel;
  **/
 public class MainFrame extends JFrame {
     private JPanel mainPanel;
+    private JPanel topPanel;
+    private JButton 院系管理Button;
+    private JButton 班级管理Button;
+    private JButton 学生管理Button;
+    private JButton 奖惩管理Button;
+    private JPanel centerPanel;
+    private JPanel departmentPanel;
+    private JPanel classPanel;
+    private JPanel studentPanel;
+    private JPanel rewardPanel;
 
-    public MainFrame(){
-        this.setTitle("MainFrame");
+    private final  CardLayout c;
+
+
+    public MainFrame() {
+        init();
+        c = new CardLayout();
+        centerPanel.setLayout(c);
+        centerPanel.add("1", departmentPanel);
+        centerPanel.add("2", classPanel);
+        centerPanel.add("3", studentPanel);
+        centerPanel.add("4", rewardPanel);
+        院系管理Button.addActionListener(e -> {
+            c.show(centerPanel, "1");
+        });
+        班级管理Button.addActionListener(e -> {
+            c.show(centerPanel, "2");
+        });
+        学生管理Button.addActionListener(e -> {
+            c.show(centerPanel, "3");
+        });
+        奖惩管理Button.addActionListener(e -> {
+            c.show(centerPanel, "4");
+        });
+    }
+    public void  init(){
         this.setContentPane(mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
